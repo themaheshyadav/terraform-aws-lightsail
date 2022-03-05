70d10b7b6f5539db081edba8f355746f979d8e6c
Mahesh Yadav <85216933+themaheshyadav@users.noreply.github.com>
2022-03-05 07:25:47 +0530

    Update _example.tf

--- example/_example.tf
+++ example/_example.tf
@@ -9,7 +9,7 @@ module "lightsail" {
   environment          = "test"
   label_order          = ["name", "environment"]
   key_pair_name  = "mahesh-keypair"
-  public_key  = "ssh-rsa AAAAB3NzaC1ycwZryh/C9mcDEMraROnQrL2E0iddww66LmybiH1qJH9zt3VIYOxhlw8xB2EwHNGqxFU9amTBsN+jhUHHvcdLGqMQ3sGxfvLymaesX/a/MR+Au7c6MFCQ1T1mxkBFfDgsRQNLvKW3o8U8T2tGJ15G24SvTvVwAKJSu0WkwwIAW+KF8RvJj1n1P1Rj+VXQSo4V0O2omd33OlzZmAucGKQF1l48uya9Im9n5Zmqk6UxhESBIP6G8GI/FduGpPwOm/xjxalmBilZ1RnIf96QwMnoRbZsS6rE1371FqFJwh6A/TEX+m0wHsGu/Sg1V8jVEe64AEGGUy9maWPMpMB9PqnnzIs4X9ndmuKkTqZbhUMV7M= mahesh@mahesh"
+  public_key  = "ssh-rsa AAAAB3NzaC1ycwZryh/C9mcDEMGxfvLymaesX/a/MR+Au7c6MFCQ1T1mxkBFfDgsRQNLvKW3o8U8T2tGJ15G24SvTvVwAKJSu0WkwwIAW+KF8RvJj1n1P1Rj+VXQSo4V0O2omd33OlzZmAucGKQF1l48uya9Im9n5Zmqk6UxhESBIP6G8GI/FduGpPwOm/xjxalmBilZ1RnIf96QwMnoRbZsS6rE1371FqFJwh6A/TEX+m0wHsGu/Sg1V8jVEe64AEGGUy9maWPMpMB9PqnnzIs4X9ndmuKkTqZbhUMV7M= mahesh@mahesh"
   use_default_key_pair = false
   port_info            = [
     {
